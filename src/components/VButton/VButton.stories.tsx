90981905a301cd213074fcd1ae4480928e5e4ded
carnold84 <dev.aura.dm@gmail.com>
2022-11-05 23:07:52 +1300

    Added Storybook

---
+++ src/components/VButton/VButton.stories.tsx
@@ -0,0 +1,23 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import VButton from './VButton';
+
+export default {
+  title: 'Components/VButton',
+  component: VButton,
+} as ComponentMeta<typeof VButton>;
+
+const Template: ComponentStory<typeof VButton> = (args) => (
+  <VButton {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  children: 'Button',
+  isPrimary: true,
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  children: 'Button',
+};
--- src/components/VButton/VButton.tsx
+++ src/components/VButton/VButton.tsx
@@ -1,20 +1,62 @@
+import { ReactNode } from 'react';
 import styled from 'styled-components';
 
-const VButton = styled.button`
-  border: none;
+interface Props {
+  children: ReactNode;
+  /**
+   * Is this the principal call to action on the page?
+   */
+  isPrimary?: boolean;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+const Wrapper = styled.button`
+  background-color: #ffffff;
+  border: 1px solid #cccccc;
   border-radius: 0.5rem;
-  background-color: #186faf;
-  color: hsl(0deg, 0%, 98%);
+  color: #222222;
   padding: 0.75rem;
   cursor: pointer;
+
   &:hover {
-    background-color: #0a558c;
+    background-color: #eeeeee;
   }
+
   &:focus {
     outline: none;
     box-shadow: 0 0 0 2px #62b0e8;
-    background-color: #0a558c;
+  }
+
+  &.is_primary {
+    background-color: #186faf;
+    border: 1px solid #186faf;
+    color: #ffffff;
+
+    &:hover {
+      background-color: #0a558c;
+      border: 1px solid #0a558c;
+    }
+
+    &:focus {
+      outline: none;
+      box-shadow: 0 0 0 2px #62b0e8;
+      background-color: #0a558c;
+    }
   }
 `;
+
+/**
+ * Primary UI component for user interaction
+ */
+const VButton = ({ children, isPrimary, onClick }: Props) => {
+  return (
+    <Wrapper className={isPrimary ? 'is_primary' : undefined} onClick={onClick}>
+      {children}
+    </Wrapper>
+  );
+};
 
 export default VButton;
